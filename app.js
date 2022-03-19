084678c2e3b94184904f1291373e7985c892e5d2
Yatharth Dwivedi <yatharthdwivedi1@gmail.com>
2022-03-19 17:38:24 +0530

    Added a procfile, gitignore and modified somethings

--- app.js
+++ app.js
@@ -149,6 +149,14 @@ app.post("/delete", function(req, res) {
  
 })
 
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+
 // console.log(module.exports);
 app.listen(3000, function() {
     console.log("Running");
